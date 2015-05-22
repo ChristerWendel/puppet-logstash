a09de64f33bd9bfd8c420fbb35ed0907230dc557
Christer Wendel <christer.wendel@altrusoft.se>
2015-05-22 20:06:29 +0200

    Bumped Logstash version

--- manifests/install.pp
+++ manifests/install.pp
@@ -5,7 +5,7 @@ class logstash::install {
   include aptrepo
 
   aptrepo::addrepo { 'logstash':
-    location => 'http://packages.elasticsearch.org/logstash/1.4/debian/',
+    location => 'http://packages.elasticsearch.org/logstash/1.5/debian/',
     key      => 'http://packages.elasticsearch.org/GPG-KEY-elasticsearch',
   } -> Package['logstash']
 
--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -5,7 +5,7 @@ describe 'logstash', :type => :class do
 
   context 'with default parameters' do
     it { should contain_aptrepo__addrepo('logstash')
-        .with_location('http://packages.elasticsearch.org/logstash/1.4/debian/')
+        .with_location('http://packages.elasticsearch.org/logstash/1.5/debian/')
         .with_key('http://packages.elasticsearch.org/GPG-KEY-elasticsearch')
     }
 
